7e5b3f4da360ee0904be27d1f57ee8107bd02326
Gekaku <travis.blanks48@gmail.com>
2026-02-27 03:20:05 -0600

    trying again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -201,7 +201,7 @@ const config = {
   themes: [
     // ... Your other themes.
     [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
+      require("@easyops-cn/docusaurus-search-local"),
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
         // ... Your options.
